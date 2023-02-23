0f57a5a63e6ac9b1c9eb1858198bcdba89a7b4ac
emacabrera <pemanuel.cabrera@gmail.com>
2023-02-23 16:46:40 -0300

    add footer

--- src/app/home/components/home.component.ts
+++ src/app/home/components/home.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 
-import { catchError, ignoreElements, Observable, of } from 'rxjs';
+import { catchError, filter, ignoreElements, map, Observable, of } from 'rxjs';
 
 import { Episode } from '@models/episode.model';
 import { ApiService } from '@services/api.service';
+import { FavService } from '@app/app/services/fav.service';
 
 @Component({
   selector: 'app-home',
@@ -16,7 +17,7 @@ export class HomeComponent implements OnInit {
   episodes$: Observable<Episode[]>;
   episodesError$: Observable<Error>;
 
-  constructor(private apiService: ApiService) {}
+  constructor(private apiService: ApiService, private favServie: FavService) {}
 
   ngOnInit(): void {
     this.episodes$ = this.apiService.getEpisodes();
@@ -24,5 +25,20 @@ export class HomeComponent implements OnInit {
       ignoreElements(),
       catchError((err: Error) => of(err))
     );
+  }
+
+  showAll(): void {
+    this.episodes$ = this.apiService.getEpisodes();
+  }
+
+  showFavs(): void {
+    const favs = this.favServie.getFavs();
+    this.episodes$ = this.apiService
+      .getEpisodes()
+      .pipe(
+        map((episodes) =>
+          episodes.filter((episode) => favs.includes(episode.id))
+        )
+      );
   }
 }
