a75c6a09d631071a532a4cbb4d73c099364b1499
emacabrera <pemanuel.cabrera@gmail.com>
2023-03-04 10:54:34 -0300

    set env variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
+/**
+ * Idealy you would like to get these values from your backend
+ * but for the sake of the exercise we are just declaring all of them in here.
+ */
 export const environment = {
   production: false,
   apiUrl: 'https://api.spotify.com/v1',
@@ -10,12 +10,3 @@ export const environment = {
   VITE_REDIRECT_URI: 'http://localhost:4200/login',
   VITE_AUTH_ENDPOINT: 'https://accounts.spotify.com/authorize',
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
